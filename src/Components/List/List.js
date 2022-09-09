1c8397e04f0141d440b8af39c6d3b70ba93b346f
Katie Walters <katie_walters@live.co.uk>
2022-09-09 21:06:51 +0100

    add div on hover

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,18 +1,34 @@
+import {useState} from 'react';
 import Navbar from "../Navbar/Navbar";
 // import pudding from "../../Data/Pudding.js"
 // import vegan from "../../Data/Vegan.js"
 import indian from "../../Data/Indian.js";
 
 export default function List() {
+    const [isHovering, setIsHovering] = useState(false);
 
-
+    const handleMouseOver = () => {
+        setIsHovering(true);
+        console.log('hovering')
+      };
+    
+      const handleMouseOut = () => {
+        setIsHovering(false);
+      };
 
     const renderRecipes = (indian) =>
     indian.map((item) => {
         const { id, title, image } = item;
         return (
           <div key={id}>
+          <div onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
             <h2>{title}</h2>
+          </div>
+          {isHovering && (
+          <div>
+            <h2>Only visible when hovering div</h2>
+          </div>
+        )}
             <img alt={title} src={image} />
           </div>
         );
@@ -22,6 +38,11 @@ export default function List() {
 return (
       <><Navbar /><div>
         {renderRecipes(indian)}
+        {isHovering && (
+          <div>
+            <h2>Only visible when hovering div</h2>
+          </div>
+        )}
     </div></>
     );
   }
