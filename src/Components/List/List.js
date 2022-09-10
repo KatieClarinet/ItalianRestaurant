1fdce5cf5c3266a453b916b4a8cfadffea83df07
Katie Walters <katie_walters@live.co.uk>
2022-09-10 23:11:21 +0100

    shows different dietary needs when hovering on different titles

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -2,13 +2,19 @@ import {useState} from 'react';
 import Navbar from "../Navbar/Navbar";
 // import pudding from "../../Data/Pudding.js"
 // import vegan from "../../Data/Vegan.js"
+import indianRecipe from '../../Data/IndianRecipe.js';
 import indian from "../../Data/Indian.js";
+import "./List.css"
 export default function List() {
     const [isHovering, setIsHovering] = useState(false);
-    const handleMouseOver = () => {
+    const [hoveredRecipe, setHoveredRecipe] = useState("")
+
+    const handleMouseOver = (event) => {
         setIsHovering(true);
+        setHoveredRecipe(event.target.innerText)
         console.log('hovering')
       };
+      console.log(hoveredRecipe)
     
       const handleMouseOut = () => {
         setIsHovering(false);
@@ -21,24 +27,51 @@ export default function List() {
           <div onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} >
             <h2>{title}</h2>
           </div>
-          {isHovering && (
-          <div className="right">
+           {/* {isHovering && ( */}
+           {/* <div className="right"> 
           <p>Working</p>
-            <h2>{id}</h2>
-          </div>
-        )}
+            <h2>{id}</h2> 
+          </div> */}
+        
+        {/* } */}
             <img alt={title} src={image} />
             <br />
             <br />
           </div>
         );
       });
+
+      const recipeInfo = (indianRecipe, hoveredRecipe) =>
+      indianRecipe.map((item) => {
+          console.log(item)
+          console.log(item.title)
+          console.log(hoveredRecipe)
+          if (hoveredRecipe === item.title) {
+            console.log(hoveredRecipe)
+              return (
+                  <>
+                  <div key={item.id}>
+                  {item.title}
+                  <br />
+                  Gluten Free: {item.glutenFree.toString()}
+                  <br />
+                  Vegan: {item.vegan.toString()}
+                  </div>
+                  </>
+              )
+          } else {
+              return null
+          }
+      })
   
 return (
       <><Navbar />
       <div className="container">
       <div className="left">
         {renderRecipes(indian)}
+    </div>
+    <div className="right">
+    {recipeInfo(indianRecipe, hoveredRecipe)}
     </div>
       </div>
     </>
