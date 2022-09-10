83a952b4d96313c0ac0a02f13b9d55b2766696fa
Katie Walters <katie_walters@live.co.uk>
2022-09-10 22:46:16 +0100

    reverted to earlier version

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,88 +1,46 @@
-import { useState } from "react";
+import {useState} from 'react';
 import Navbar from "../Navbar/Navbar";
 // import pudding from "../../Data/Pudding.js"
 // import vegan from "../../Data/Vegan.js"
 import indian from "../../Data/Indian.js";
-import indianRecipe from "../../Data/IndianRecipe.js";
-import "./List.css"
-
 export default function List() {
     const [isHovering, setIsHovering] = useState(false);
-    const [recipe, setRecipe] = useState("");
-    // const [check, setCheck] = useState({})
-
-    const handleMouseOver = (event) => {
+    const handleMouseOver = () => {
         setIsHovering(true);
-        setRecipe(event.target.innerText)
+        console.log('hovering')
       };
-console.log(recipe)
-    const handleMouseOut = () => {
+    
+      const handleMouseOut = () => {
         setIsHovering(false);
-        setRecipe(0)
-    };
-
-    const renderTitles = (indian) =>
-    console.log("indian", indian)
-        indian.map((item) => {
-            const { id, title, image } = item;
-            // console.log(item)
-            return (
-                <>
-                    <div key={id}>
-                        <div
-                            onMouseOver={handleMouseOver}
-                            onMouseOut={handleMouseOut}
-                        >
-                            <h2>{title}</h2>
-                         </div>
-                         {/* {isHovering &&
-                        displayRecipeDetails(indianRecipe)} */}
-
-                        </div>
-
-                        <img alt={title} src={image} />
-                        <br />
-                        <br />
-                    
-                </>
-            );
-        });
-
-    // const renderRecipes = (indianRecipe, recipe) => {
-    //   const result = Object.keys(indianRecipe).map(key => {
-    //     return {[key]: indianRecipe[key]};
-    //   });
-    //   console.log("result", result)
-    // }
-
-    // const displayRecipeDetails = (indianRecipe) => {
-    //   const check = (Object.values(indianRecipe)) 
-    //   console.log('hovering')
-    //    console.log("recipe", recipe)
-    //   console.log("check", check)
-    //   console.log("check0title", check[0].title)
-    //   // check.map((item) => {
-    //   //   return (
-    //   //     <div>{item.title}</div>
-    //   //   )
-
-    //   // })
-    //   console.log("state", check)
-    // }
-
-
-    return (
-        <>
-            <Navbar />
-            <div className="container">
-                <div className="left">{renderTitles(indian)}</div>
-                <div className="right">
-                         {/* {isHovering &&
-                        displayRecipeDetails(indianRecipe)} */}
-
-                        </div>
-                 
-            </div>
-        </>
+      };
+    const renderRecipes = (indian) =>
+    indian.map((item) => {
+        const { id, title, image } = item;
+        return (
+          <div key={id} >
+          <div onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} >
+            <h2>{title}</h2>
+          </div>
+          {isHovering && (
+          <div className="right">
+          <p>Working</p>
+            <h2>{id}</h2>
+          </div>
+        )}
+            <img alt={title} src={image} />
+            <br />
+            <br />
+          </div>
+        );
+      });
+  
+return (
+      <><Navbar />
+      <div className="container">
+      <div className="left">
+        {renderRecipes(indian)}
+    </div>
+      </div>
+    </>
     );
-}
+  }
