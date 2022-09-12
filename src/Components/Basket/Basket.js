2c42af27452adea07f3b408133abc96219c1e4f3
Katie Walters <katie_walters@live.co.uk>
2022-09-12 12:47:35 +0100

    updated styling to rows with flex wrap

--- src/Components/Basket/Basket.js
+++ src/Components/Basket/Basket.js
@@ -1,10 +1,127 @@
-import Navbar from "../Navbar/Navbar";
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment, useRef, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
 
-export default function Basket() {
-    return (
-      <div>
-      <Navbar />
-        <h2>Basket</h2>
-      </div>
-    );
-  }
+export default function Example() {
+  const [open, setOpen] = useState(false)
+
+  const cancelButtonRef = useRef(null)
+
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                    </div>
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
+                        Dietary Information
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          Are you sure you want to deactivate your account? All of your data will be permanently
+                          removed. This action cannot be undone.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                  <button
+                    type="button"
+                    className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => setOpen(false)}
+                  >
+                    Deactivate
+                  </button>
+                  <button
+                    type="button"
+                    className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => setOpen(false)}
+                    ref={cancelButtonRef}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  )
+}
+
+
+
+// import Navbar from "../Navbar/Navbar";
+// import spices from "../../Assets/Spices.jpg"
+
+// export default function Basket() {
+//     return (
+//       <><div>
+//         <Navbar />
+//         <div class="flex flex-wrap">
+
+//         <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+//           <a href="#">
+//             <img class="rounded-t-lg" src={spices} alt="" />
+//           </a>
+//           <div class="p-5">
+//               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+//             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+//             <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+//               Read more
+//               <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+//             </a>
+//           </div>
+//         </div>
+  
+//       <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+//           <a href="#">
+//             <img class="rounded-t-lg" src={spices} alt="" />
+//           </a>
+//           <div class="p-5">
+//             <a href="#">
+//               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+//             </a>
+//             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+//             <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+//               Read more
+//               <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+//             </a>
+//           </div>
+//         </div>
+//         </div>
+       
+//       </div>
+//         </>
+//     );
+//   }
--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -4,7 +4,6 @@ import Navbar from "../Navbar/Navbar";
 // import vegan from "../../Data/Vegan.js"
 import indianRecipe from '../../Data/IndianRecipe.js';
 import indian from "../../Data/Indian.js";
-import "./List.css"
 export default function List() {
     const [isHovering, setIsHovering] = useState(false);
     const [hoveredRecipe, setHoveredRecipe] = useState("")
@@ -14,7 +13,7 @@ export default function List() {
         setHoveredRecipe(event.target.innerText)
         console.log('hovering')
       };
-      console.log(hoveredRecipe)
+      // console.log(hoveredRecipe)
     
       const handleMouseOut = () => {
         setIsHovering(false);
@@ -23,27 +22,30 @@ export default function List() {
     indian.map((item) => {
         const { id, title, image } = item;
         return (
+          // <div class="flex flex-wrap">
           <div key={id} class="max-w-sm rounded overflow-hidden shadow-lg">
             <img alt={title} src={image} class="w-full" />
           <div class="px-6 py-4"
           onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} >
             <div class="font-bold text-xl mb-2">{title}</div>
-            <p class="text-gray-700 text-base">More info</p>
+            <div class="text-gray-700 text-base">More info</div>
           </div>
             <div class="px-6 pt-4 pb-2">
             <button type="submit" class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Add to basket</button>
             </div>
           </div>
+          // </div>
+        
         );
       });
 
       const recipeInfo = (indianRecipe, hoveredRecipe) =>
       indianRecipe.map((item) => {
-          console.log(item)
-          console.log(item.title)
-          console.log(hoveredRecipe)
+          // console.log(item)
+          // console.log(item.title)
+          // console.log(hoveredRecipe)
           if (hoveredRecipe === item.title) {
-            console.log(hoveredRecipe)
+            // console.log(hoveredRecipe)
               return (
                   <>
                   <div key={item.id}>
@@ -62,14 +64,15 @@ export default function List() {
   
 return (
       <><Navbar />
-      <div className="container">
-      <div className="left">
-        {renderRecipes(indian)}
-    </div>
-    <div className="right">
+      
+      <div>
     {recipeInfo(indianRecipe, hoveredRecipe)}
     </div>
-      </div>
+      <div class="flex flex-wrap px-8 space-x-4 space-y-4">
+        {renderRecipes(indian)}
+    </div>
+    
+      
     </>
     );
   }
